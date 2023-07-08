65ab3ddbf634a3c43a3181b9740bccd647a3e8f9
Darryl-Shi <221474B@student.hci.edu.sg>
2023-07-08 16:15:34 +0800

    removed dataset grouping

--- finetune/finetune.py
+++ finetune/finetune.py
@@ -441,26 +441,8 @@ def build_dataloaders():
         remove_columns=["content"],
     )
 
-    block_size = CFG.SEQ_LEN
-
-    # Main data processing function that will concatenate all texts from our dataset and generate chunks of block_size.
-    def group_texts(examples):
-        # Concatenate all texts.
-        concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
-        total_length = len(concatenated_examples[list(examples.keys())[0]])
-        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-        # customize this part to your needs.
-        if total_length >= block_size:
-            total_length = (total_length // block_size) * block_size
-        # Split by chunks of max_len.
-        result = {
-            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-            for k, t in concatenated_examples.items()
-        }
-        return result
-
     train_dataset = tokenized_dataset.map(
-        group_texts, batched=True, num_proc=CFG.NUM_CPU,
+        batched=True, num_proc=CFG.NUM_CPU,
     )
 
     return train_dataset
